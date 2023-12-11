9a3b9ecbc22bbe93333840806cc48130845a69f4
JFeintzeig <JFeintzeig@users.noreply.github.com>
2023-12-11 15:05:37 -0500

    part 2 done; what a mess

--- 10/main.go
+++ 10/main.go
@@ -10,14 +10,14 @@ type Coord struct {
 	i, j int
 }
 
-type Node interface {
+type NodeTraverser interface {
 	before() (i, j int)
 	after() (i, j int)
-	//getinsideba() (i, j int)
-	//getinsideab() (i, j int)
+	getinsideba() []Coord
+	getinsideab() []Coord
 }
 
-func GetNext(n Node, i int, j int) (ni, nj int) {
+func GetNext(n *Node, i int, j int) (ni, nj int) {
 	iBefore, jBefore := n.before()
 	iAfter, jAfter := n.after()
 	if iBefore == i && jBefore == j {
@@ -31,146 +31,203 @@ func GetNext(n Node, i int, j int) (ni, nj int) {
 	}
 }
 
-//func GetInside(n Node, i int, j int) (ini, inj int) {
-//    iBefore, jBefore := n.before()
-//    iAfter, jAfter := n.after()
-//    if iBefore == i && jBefore == j {
-//        return n.getinsideba()
-//    } else if iAfter == i && jAfter == j {
-//        return n.getinsideab()
-//    } else {
-//        panic("problem")
-//        return
-//    }
-//}
+func GetInside(n *Node, i int, j int) []Coord {
+    iBefore, jBefore := n.before()
+    iAfter, jAfter := n.after()
+    if iBefore == i && jBefore == j {
+        return n.getinsideba()
+    } else if iAfter == i && jAfter == j {
+        return n.getinsideab()
+    } else {
+        panic("problem")
+        return make([]Coord, 0)
+    }
+}
 
-type Pipe struct {
-	i, j int
+type Node struct {
+    NodeTraverser
 	name string
 }
 
-func (n *Pipe) before() (i, j int) {
+type Pipe struct {
+    Coord
+}
+
+func (n Pipe) before() (i, j int) {
 	return n.i - 1, n.j
 }
 
-func (n *Pipe) after() (i, j int) {
+func (n Pipe) after() (i, j int) {
 	return n.i + 1, n.j
+}
+
+func (n Pipe) getinsideba() []Coord {
+    return []Coord{Coord{n.i, n.j-1}}
+}
+
+func (n Pipe) getinsideab() []Coord {
+    return []Coord{Coord{n.i, n.j+1}}
 }
 
 type Dash struct {
-	i, j int
-	name string
+	Coord
 }
 
-func (n *Dash) before() (i, j int) {
+func (n Dash) before() (i, j int) {
 	return n.i, n.j - 1
 }
 
-func (n *Dash) after() (i, j int) {
+func (n Dash) after() (i, j int) {
 	return n.i, n.j + 1
+}
+
+func (n Dash) getinsideba() []Coord {
+    return []Coord{Coord{n.i+1, n.j}}
+}
+
+func (n Dash) getinsideab() []Coord {
+    return []Coord{Coord{n.i-1, n.j}}
 }
 
 type L struct {
-	i, j int
-	name string
+	Coord
 }
 
-func (n *L) before() (i, j int) {
+func (n L) before() (i, j int) {
 	return n.i - 1, n.j
 }
 
-func (n *L) after() (i, j int) {
+func (n L) after() (i, j int) {
 	return n.i, n.j + 1
+}
+
+func (n L) getinsideba() []Coord {
+    return []Coord{Coord{n.i, n.j-1},Coord{n.i+1, n.j}}
+}
+
+func (n L) getinsideab() []Coord {
+    return []Coord{Coord{n.i, n.j+1}}
 }
 
 type J struct {
-	i, j int
-	name string
+    Coord
 }
 
-func (n *J) before() (i, j int) {
+func (n J) before() (i, j int) {
 	return n.i - 1, n.j
 }
 
-func (n *J) after() (i, j int) {
+func (n J) after() (i, j int) {
 	return n.i, n.j - 1
+}
+
+func (n J) getinsideba() []Coord {
+    return []Coord{Coord{n.i, n.j-1}}
+}
+
+func (n J) getinsideab() []Coord {
+    return []Coord{Coord{n.i+1, n.j}, Coord{n.i, n.j+1}}
 }
 
 type Seven struct {
-	i, j int
-	name string
+	Coord
 }
 
-func (n *Seven) before() (i, j int) {
+func (n Seven) before() (i, j int) {
 	return n.i, n.j - 1
 }
 
-func (n *Seven) after() (i, j int) {
+func (n Seven) after() (i, j int) {
 	return n.i + 1, n.j
+}
+
+func (n Seven) getinsideba() []Coord {
+    return []Coord{Coord{n.i, n.j-1}}
+}
+
+func (n Seven) getinsideab() []Coord {
+    return []Coord{Coord{n.i, n.j+1}, Coord{n.i-1, n.j}}
 }
 
 type F struct {
-	i, j int
-	name string
+	Coord
 }
 
-func (n *F) before() (i, j int) {
+func (n F) before() (i, j int) {
 	return n.i + 1, n.j
 }
 
-func (n *F) after() (i, j int) {
+func (n F) after() (i, j int) {
 	return n.i, n.j + 1
 }
 
-type Period struct {
-	i, j int
-	name string
+func (n F) getinsideba() []Coord {
+    return []Coord{Coord{n.i, n.j+1}}
 }
 
-func (n *Period) before() (i, j int) {
+func (n F) getinsideab() []Coord {
+    return []Coord{Coord{n.i, n.j-1}, Coord{n.i-1, n.j}}
+}
+
+type Period struct {
+	Coord
+}
+
+func (n Period) before() (i, j int) {
 	panic("no before() for Period")
 	return
 }
 
-func (n *Period) after() (i, j int) {
+func (n Period) after() (i, j int) {
 	panic("no after() for Period")
 	return
 }
 
-func NewNode(i int, j int, s string) Node {
+func (n Period) getinsideba() []Coord {
+    panic("no inside for Period")
+    return make([]Coord, 0)
+}
+
+func (n Period) getinsideab() []Coord {
+    panic("no inside for Period")
+    return make([]Coord, 0)
+}
+
+func NewNode(i int, j int, s string) *Node {
+    c := Coord{i, j}
 	switch s {
 	case "|":
-		return &Pipe{i, j, s}
+		return &Node{Pipe{c}, s}
 	case "-":
-		return &Dash{i, j, s}
+		return &Node{Dash{c}, s}
 	case "L":
-		return &L{i, j, s}
+		return &Node{L{c}, s}
 	case "J":
-		return &J{i, j, s}
+		return &Node{J{c}, s}
 	case "7":
-		return &Seven{i, j, s}
+		return &Node{Seven{c}, s}
 	case "F":
-		return &F{i, j, s}
+		return &Node{F{c}, s}
 	case ".":
-		return &Period{i, j, s}
+		return &Node{Period{c}, s}
 	// NB: we replace this later, hardcoded + dumb
 	case "S":
-		return &Dash{i, j, s}
+		return &Node{Dash{c}, s}
 	default:
 		panic("uh oh unknown char")
-		return &Pipe{i, j, s}
+		return &Node{Pipe{c}, s}
 	}
 }
 
-func LoadFile(fileName string) ([][]Node, int, int) {
+func LoadFile(fileName string) ([][]*Node, int, int) {
 	file, _ := os.ReadFile(fileName)
 
 	lines := strings.Split(strings.TrimSpace(string(file)), "\n")
 
 	var iStart, jStart int
-	nodeMatrix := make([][]Node, 0)
+	nodeMatrix := make([][]*Node, 0)
 	for i, line := range lines {
-		nodeRow := make([]Node, 0)
+		nodeRow := make([]*Node, 0)
 		chars := strings.Split(line, "")
 		for j, char := range chars {
 			if char == "S" {
@@ -183,68 +240,107 @@ func LoadFile(fileName string) ([][]Node, int, int) {
 	}
 
 	// NB: replace start node, hardcoding values in my files
-	if fileName == "test1a.data" || fileName == "test1b.data" {
-		nodeMatrix[iStart][jStart] = &F{iStart, jStart, "S: F"}
-	} else if fileName == "test2a.data" || fileName == "test2b.data" {
-		nodeMatrix[iStart][jStart] = &F{iStart, jStart, "S: F"}
+	if fileName == "test3c.data" {
+		nodeMatrix[iStart][jStart] = &Node{Seven{Coord{iStart, jStart}}, "S: 7"}
+	} else if strings.Contains(fileName, "test") {
+		nodeMatrix[iStart][jStart] = &Node{F{Coord{iStart, jStart}}, "S: F"}
 	} else if fileName == "input.data" {
-		nodeMatrix[iStart][jStart] = &Dash{iStart, jStart, "S: -"}
+		nodeMatrix[iStart][jStart] = &Node{Dash{Coord{iStart, jStart}}, "S: -"}
 	}
 
 	return nodeMatrix, iStart, jStart
 }
 
-func TraverseLoop(nodeMatrix [][]Node, iStart int, jStart int, iPrev int, jPrev int) (int, map[Coord]bool) {
+func TraverseLoop(nodeMatrix [][]*Node, iStart int, jStart int, iPrev int, jPrev int) (int, map[Coord]bool, map[Coord]bool) {
 	i := iStart
 	j := jStart
 	nodePartOfLoop := make(map[Coord]bool)
+    insides := make(map[Coord]bool)
 
 	nSteps := 0
 	for !(i == iStart && j == jStart && nSteps != 0) {
 		node := nodeMatrix[i][j]
 		iNew, jNew := GetNext(node, iPrev, jPrev)
+        for _, val := range GetInside(node, iPrev, jPrev) {
+            insides[val] = true
+        }
+
 		iPrev, jPrev = i, j
 		i, j = iNew, jNew
 		nSteps += 1
 
 		nodePartOfLoop[Coord{i, j}] = true
 	}
-	return nSteps, nodePartOfLoop
+	return nSteps, nodePartOfLoop, insides
+}
+
+func RecursiveGetNeighbors(coords map[Coord]bool, loop map[Coord]bool) map[Coord]bool {
+    neighbors := make(map[Coord]bool)
+    for c, _ := range coords {
+        neighbors[c] = true
+        north := Coord{c.i-1, c.j}
+        south := Coord{c.i+1, c.j}
+        east := Coord{c.i, c.j+1}
+        west := Coord{c.i, c.j-1}
+        for _, n:= range []Coord{north, south, east, west} {
+            _, neighborOnLoop := loop[n]
+            if !neighborOnLoop {
+                neighbors[n] = true
+            } else {
+            }
+        }
+    }
+    if len(neighbors) == len(coords) {
+        return neighbors
+    } else {
+        return RecursiveGetNeighbors(neighbors, loop)
+    }
 }
 
 func main() {
+	//nodeMatrix, iStart, jStart := LoadFile("test3c.data")
 	nodeMatrix, iStart, jStart := LoadFile("input.data")
-	fmt.Printf("(%d, %d)\n", iStart, jStart)
+	fmt.Printf("Start: (%d, %d)\n", iStart, jStart)
 
 	// hardcoding the "entry direction" for test data
-	//nSteps1, nodePartOfLoop := TraverseLoop(nodeMatrix, iStart, jStart, iStart+1, jStart)
-	nSteps1, nodePartOfLoop := TraverseLoop(nodeMatrix, iStart, jStart, iStart, jStart-1)
+	//nSteps1, nodePartOfLoop, insides := TraverseLoop(nodeMatrix, iStart, jStart, iStart, jStart-1)
+	nSteps1, nodePartOfLoop, insides := TraverseLoop(nodeMatrix, iStart, jStart, iStart, jStart-1)
 	fmt.Printf("Part 1 Direction 1: %d\n", nSteps1)
-	//nSteps2, nodePartOfLoop := TraverseLoop(nodeMatrix, iStart, jStart, iStart, jStart+1)
-	nSteps2, nodePartOfLoop := TraverseLoop(nodeMatrix, iStart, jStart, iStart, jStart+1)
-	fmt.Printf("Part 1 Direction 2: %d\n", nSteps2)
 	fmt.Printf("Part 1 Answer: %d\n", nSteps1/2)
 
-	// part 2: first visualize nodes that are not in the loop
-	// TODO: if i can implement getinsideab() and getinsideba()
-	// for ** one ** direction around the loop (clockwise?)
-	// AND figure out which direction represents clockwise
-	// for the real input, can i then count pixels inside?
-	// basic algo: if the x,y from getinsideab() (or ba(), whichever appropriate)
-	// is a pixel that's not part of the loop, then increment count
-	// how to deal with contiguous spaces of inside pixels? maybe
-	// iteratively apply the transformation to get new x,y's
-	// farther inside and test if they're not part of loop?
+    fmt.Printf("Insides:\n")
+    for c, _ := range insides {
+        _, ok := nodePartOfLoop[c]
+        if !ok {
+
+            m := make(map[Coord]bool)
+            m[c] = true
+            neighbors := RecursiveGetNeighbors(m, nodePartOfLoop)
+            for k, _ := range neighbors {
+                insides[k] = true
+            }
+        } else {
+            delete(insides, c)
+        }
+    }
+
 	for i, row := range nodeMatrix {
 		for j, _ := range row {
-			_, ok := nodePartOfLoop[Coord{i, j}]
+            c := Coord{i, j}
+			_, ok := nodePartOfLoop[c]
 			if ok {
 				fmt.Printf("X")
 			} else {
+                _, in := insides[c]
+                if in {
+                    fmt.Printf("I")
+                } else {
 				fmt.Printf("O")
+                }
 			}
 		}
 		fmt.Printf("\n")
 	}
-	fmt.Printf("\n")
+
+    fmt.Printf("Part 2 Answer: %d\n", len(insides))
 }
