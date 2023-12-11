33dff30cc9cc6857d483005fba2834db692333bc
JFeintzeig <JFeintzeig@users.noreply.github.com>
2023-12-10 23:30:39 -0500

    refactor part 1

--- 10/main.go
+++ 10/main.go
@@ -6,25 +6,55 @@ import (
 	"strings"
 )
 
-type Node interface {
-	next(i, j int) (ni, nj int)
+type Coord struct {
+	i, j int
 }
+
+type Node interface {
+	before() (i, j int)
+	after() (i, j int)
+	//getinsideba() (i, j int)
+	//getinsideab() (i, j int)
+}
+
+func GetNext(n Node, i int, j int) (ni, nj int) {
+	iBefore, jBefore := n.before()
+	iAfter, jAfter := n.after()
+	if iBefore == i && jBefore == j {
+		return n.after()
+	} else if iAfter == i && jAfter == j {
+		return n.before()
+	} else {
+		msg := fmt.Sprintf("problem with GetNext(): %v %d %d", n, i, j)
+		panic(msg)
+		return
+	}
+}
+
+//func GetInside(n Node, i int, j int) (ini, inj int) {
+//    iBefore, jBefore := n.before()
+//    iAfter, jAfter := n.after()
+//    if iBefore == i && jBefore == j {
+//        return n.getinsideba()
+//    } else if iAfter == i && jAfter == j {
+//        return n.getinsideab()
+//    } else {
+//        panic("problem")
+//        return
+//    }
+//}
 
 type Pipe struct {
 	i, j int
 	name string
 }
 
-func (n *Pipe) next(i, j int) (ni, nj int) {
-	if i == n.i-1 && j == n.j {
-		return n.i + 1, n.j
-	} else if i == n.i+1 && j == n.j {
-		return n.i - 1, n.j
-	} else {
-		msg := fmt.Sprintf("(%d, %d) is not valid for %s at (%d, %d)", i, j, n.name, n.i, n.j)
-		panic(msg)
-		return
-	}
+func (n *Pipe) before() (i, j int) {
+	return n.i - 1, n.j
+}
+
+func (n *Pipe) after() (i, j int) {
+	return n.i + 1, n.j
 }
 
 type Dash struct {
@@ -32,16 +62,12 @@ type Dash struct {
 	name string
 }
 
-func (n *Dash) next(i, j int) (ni, nj int) {
-	if i == n.i && j == n.j-1 {
-		return n.i, n.j + 1
-	} else if i == n.i && j == n.j+1 {
-		return n.i, n.j - 1
-	} else {
-		msg := fmt.Sprintf("(%d, %d) is not valid for %s at (%d, %d)", i, j, n.name, n.i, n.j)
-		panic(msg)
-		return
-	}
+func (n *Dash) before() (i, j int) {
+	return n.i, n.j - 1
+}
+
+func (n *Dash) after() (i, j int) {
+	return n.i, n.j + 1
 }
 
 type L struct {
@@ -49,16 +75,12 @@ type L struct {
 	name string
 }
 
-func (n *L) next(i, j int) (ni, nj int) {
-	if i == n.i-1 && j == n.j {
-		return n.i, n.j + 1
-	} else if i == n.i && j == n.j+1 {
-		return n.i - 1, n.j
-	} else {
-		msg := fmt.Sprintf("(%d, %d) is not valid for %s at (%d, %d)", i, j, n.name, n.i, n.j)
-		panic(msg)
-		return
-	}
+func (n *L) before() (i, j int) {
+	return n.i - 1, n.j
+}
+
+func (n *L) after() (i, j int) {
+	return n.i, n.j + 1
 }
 
 type J struct {
@@ -66,16 +88,12 @@ type J struct {
 	name string
 }
 
-func (n *J) next(i, j int) (ni, nj int) {
-	if i == n.i-1 && j == n.j {
-		return n.i, n.j - 1
-	} else if i == n.i && j == n.j-1 {
-		return n.i - 1, n.j
-	} else {
-		msg := fmt.Sprintf("(%d, %d) is not valid for %s at (%d, %d)", i, j, n.name, n.i, n.j)
-		panic(msg)
-		return
-	}
+func (n *J) before() (i, j int) {
+	return n.i - 1, n.j
+}
+
+func (n *J) after() (i, j int) {
+	return n.i, n.j - 1
 }
 
 type Seven struct {
@@ -83,16 +101,12 @@ type Seven struct {
 	name string
 }
 
-func (n *Seven) next(i, j int) (ni, nj int) {
-	if i == n.i && j == n.j-1 {
-		return n.i + 1, n.j
-	} else if i == n.i+1 && j == n.j {
-		return n.i, n.j - 1
-	} else {
-		msg := fmt.Sprintf("(%d, %d) is not valid for %s at (%d, %d)", i, j, n.name, n.i, n.j)
-		panic(msg)
-		return
-	}
+func (n *Seven) before() (i, j int) {
+	return n.i, n.j - 1
+}
+
+func (n *Seven) after() (i, j int) {
+	return n.i + 1, n.j
 }
 
 type F struct {
@@ -100,16 +114,12 @@ type F struct {
 	name string
 }
 
-func (n *F) next(i, j int) (ni, nj int) {
-	if i == n.i+1 && j == n.j {
-		return n.i, n.j + 1
-	} else if i == n.i && j == n.j+1 {
-		return n.i + 1, n.j
-	} else {
-		msg := fmt.Sprintf("(%d, %d) is not valid for %s at (%d, %d)", i, j, n.name, n.i, n.j)
-		panic(msg)
-		return
-	}
+func (n *F) before() (i, j int) {
+	return n.i + 1, n.j
+}
+
+func (n *F) after() (i, j int) {
+	return n.i, n.j + 1
 }
 
 type Period struct {
@@ -117,9 +127,13 @@ type Period struct {
 	name string
 }
 
-func (n *Period) next(i, j int) (ni, nj int) {
-	msg := fmt.Sprintf("(%d, %d) is not valid for %s at (%d, %d)", i, j, n.name, n.i, n.j)
-	panic(msg)
+func (n *Period) before() (i, j int) {
+	panic("no before() for Period")
+	return
+}
+
+func (n *Period) after() (i, j int) {
+	panic("no after() for Period")
 	return
 }
 
@@ -180,33 +194,57 @@ func LoadFile(fileName string) ([][]Node, int, int) {
 	return nodeMatrix, iStart, jStart
 }
 
-func TraverseLoop(nodeMatrix [][]Node, iStart int, jStart int, iPrev int, jPrev int) int {
+func TraverseLoop(nodeMatrix [][]Node, iStart int, jStart int, iPrev int, jPrev int) (int, map[Coord]bool) {
 	i := iStart
 	j := jStart
-	// hardcoding the "entry" position for test data
+	nodePartOfLoop := make(map[Coord]bool)
+
 	nSteps := 0
 	for !(i == iStart && j == jStart && nSteps != 0) {
 		node := nodeMatrix[i][j]
-		iNew, jNew := node.next(iPrev, jPrev)
+		iNew, jNew := GetNext(node, iPrev, jPrev)
 		iPrev, jPrev = i, j
 		i, j = iNew, jNew
 		nSteps += 1
+
+		nodePartOfLoop[Coord{i, j}] = true
 	}
-	return nSteps
+	return nSteps, nodePartOfLoop
 }
 
 func main() {
-	fmt.Printf("hello world\n")
-
 	nodeMatrix, iStart, jStart := LoadFile("input.data")
 	fmt.Printf("(%d, %d)\n", iStart, jStart)
 
 	// hardcoding the "entry direction" for test data
-	//nSteps1 := TraverseLoop(nodeMatrix, iStart, jStart, iStart+1, jStart)
-	nSteps1 := TraverseLoop(nodeMatrix, iStart, jStart, iStart, jStart-1)
+	//nSteps1, nodePartOfLoop := TraverseLoop(nodeMatrix, iStart, jStart, iStart+1, jStart)
+	nSteps1, nodePartOfLoop := TraverseLoop(nodeMatrix, iStart, jStart, iStart, jStart-1)
 	fmt.Printf("Part 1 Direction 1: %d\n", nSteps1)
-	//nSteps2 := TraverseLoop(nodeMatrix, iStart, jStart, iStart, jStart+1)
-	nSteps2 := TraverseLoop(nodeMatrix, iStart, jStart, iStart, jStart+1)
+	//nSteps2, nodePartOfLoop := TraverseLoop(nodeMatrix, iStart, jStart, iStart, jStart+1)
+	nSteps2, nodePartOfLoop := TraverseLoop(nodeMatrix, iStart, jStart, iStart, jStart+1)
 	fmt.Printf("Part 1 Direction 2: %d\n", nSteps2)
 	fmt.Printf("Part 1 Answer: %d\n", nSteps1/2)
+
+	// part 2: first visualize nodes that are not in the loop
+	// TODO: if i can implement getinsideab() and getinsideba()
+	// for ** one ** direction around the loop (clockwise?)
+	// AND figure out which direction represents clockwise
+	// for the real input, can i then count pixels inside?
+	// basic algo: if the x,y from getinsideab() (or ba(), whichever appropriate)
+	// is a pixel that's not part of the loop, then increment count
+	// how to deal with contiguous spaces of inside pixels? maybe
+	// iteratively apply the transformation to get new x,y's
+	// farther inside and test if they're not part of loop?
+	for i, row := range nodeMatrix {
+		for j, _ := range row {
+			_, ok := nodePartOfLoop[Coord{i, j}]
+			if ok {
+				fmt.Printf("X")
+			} else {
+				fmt.Printf("O")
+			}
+		}
+		fmt.Printf("\n")
+	}
+	fmt.Printf("\n")
 }
